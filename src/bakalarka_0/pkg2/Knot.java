ed1f5bfe287a813a78985fa446d7b0313b0756f7
Ondřej Kmínek <>
2021-07-31 17:59:59 +0200

    Přechod na double

--- src/bakalarka_0/pkg2/Knot.java
+++ src/bakalarka_0/pkg2/Knot.java
@@ -11,19 +11,19 @@ public class Knot {
     Pendulum x; //kyvadlo pro x souřadnici
     Pendulum y; //kyvadlo pro y souřadnici
     Pendulum[] z= new Pendulum[2]; //kyvadla pro z souřadnici
-    ArrayList<BigDecimal> time1 = new ArrayList<>();
-    ArrayList<BigDecimal> time2 = new ArrayList<>();
-    ArrayList<BigDecimal> times=new ArrayList<>();
+    ArrayList<Double> time1 = new ArrayList<>();
+    ArrayList<Double> time2 = new ArrayList<>();
+    ArrayList<Double> times=new ArrayList<>();
     //pořadí bodů je závislé od vstupní proměnné(času - seřazený vzestupně)
-    BigDecimal PI = new BigDecimal("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679");
     String DT=""; //DT kód jako jeden řetězec znaků
     //kontruktory
     public Knot(){ // - bezparametrický pro testování
-        this.x=(new Pendulum(2,BigDecimal.ZERO));
-        this.y=(new Pendulum(3,PI.divide(BigDecimal.valueOf(4))));
-        this.z[0]=(new Pendulum(2,PI.divide(BigDecimal.valueOf(2))));
-        this.z[1]=(new Pendulum(1,PI.divide(BigDecimal.valueOf(4))));
+        this.x=(new Pendulum(2,0));
+        this.y=(new Pendulum(3,Math.PI/4));
+        this.z[0]=(new Pendulum(2,Math.PI/2));
+        this.z[1]=(new Pendulum(1,Math.PI/4));
         init();
+        System.out.println(DT);
     }
     
     public Knot(Pendulum x,Pendulum y,Pendulum z1,Pendulum z2){ // - 4 vstupní parametry(kyvadla)
@@ -34,7 +34,7 @@ public class Knot {
         
     } 
     
-    public Knot(int f1,BigDecimal p1,int f2,BigDecimal p2,int f3,BigDecimal p3,int f4, BigDecimal p4){ // - 8 parametrů(4 páry) každá dvojice pro jedno kyvadlo
+    public Knot(int f1,double p1,int f2,double p2,int f3,double p3,int f4, double p4){ // - 8 parametrů(4 páry) každá dvojice pro jedno kyvadlo
         this.x=new Pendulum(f1,p1);
         this.y=new Pendulum(f2,p2);
         this.z[0]=new Pendulum(f3,p3);
@@ -57,23 +57,26 @@ public class Knot {
     public void findPair1(){
         for(int k=1;k<x.f;k++){
             double k1 = y.f*k;
-            BigDecimal a = BigDecimal.valueOf(k1).divide(BigDecimal.valueOf(x.f),100,RoundingMode.FLOOR);
-            BigDecimal b = y.p.divide(this.PI,100,RoundingMode.FLOOR);
-            int bottom = 1+((a.add(b)).setScale(0, RoundingMode.FLOOR)).intValue();
-            int top = BigDecimal.valueOf(2*y.f).subtract(a).add(b).setScale(0, RoundingMode.FLOOR).intValue();
+            double a = (double)(k1/x.f);
+            double b = (double)y.p/Math.PI;
+            System.out.println(k1+" "+x.f);
+            System.out.println(a+" "+b);
+            int bottom = (int)Math.floor(1.0+(a+b));
+            int top = (int)Math.floor(2*y.f-a+b);
+            System.out.println(bottom+" "+top);
             for(int j=bottom;j<=top;j++){
                 
-                BigDecimal k2 = BigDecimal.valueOf(k).divide(BigDecimal.valueOf(x.f),100,RoundingMode.FLOOR);
-                BigDecimal j1 = BigDecimal.valueOf(j).divide(BigDecimal.valueOf(y.f),100,RoundingMode.FLOOR);
-                BigDecimal om = y.p.divide(BigDecimal.valueOf(y.f),100,RoundingMode.FLOOR);
+                double k2 = ((double)k/x.f);
+                double j1 = ((double)j/y.f);
+                double om = (y.p/y.f);
                 
-                BigDecimal t1 = k2.negate().add(j1).multiply(PI).subtract(om);
-                BigDecimal t2 = k2.add(j1).multiply(PI).subtract(om);
+                double t1 =(((-k2)+j1)*Math.PI + om);
+                double t2 =(((k2)+j1)*Math.PI + om);
+                
                 this.times.add(t1);
                 this.times.add(t2);
                 this.time1.add(t1);
                 this.time2.add(t2);
-                
             }
         }
     }
@@ -81,18 +84,19 @@ public class Knot {
     public void findPair2(){
         for(int k=1;k<y.f;k++){
             double k1 = x.f*k;
-            BigDecimal a = BigDecimal.valueOf(k1).divide(BigDecimal.valueOf(y.f),100,RoundingMode.FLOOR);
-            BigDecimal b = x.p.divide(this.PI,100,RoundingMode.FLOOR);
-            int bottom = 1+((BigDecimal.valueOf(k1).divide(BigDecimal.valueOf(y.f),100,RoundingMode.FLOOR).add(b)).setScale(0, RoundingMode.FLOOR)).intValue();
-            int top = BigDecimal.valueOf(2*x.f).subtract(a).add(b).setScale(0, RoundingMode.FLOOR).intValue();
+            double a = (double)(k1/y.f);
+            double b = (double)x.p/Math.PI;
+            int bottom = (int)Math.floor(1.0+(a+b));
+            int top = (int)Math.floor(2*x.f-a+b);
             for(int j=bottom;j<=top;j++){
                 
-                BigDecimal k2 = BigDecimal.valueOf(k).divide(BigDecimal.valueOf(y.f),100,RoundingMode.FLOOR);
-                BigDecimal j1 = BigDecimal.valueOf(j).divide(BigDecimal.valueOf(x.f),100,RoundingMode.FLOOR);
-                BigDecimal om = x.p.divide(BigDecimal.valueOf(x.f),100,RoundingMode.FLOOR);
+                double k2 = ((double)k/y.f);
+                double j1 = ((double)j/x.f);
+                double om = (x.p/x.f);
                 
-                BigDecimal t1 = k2.negate().add(j1).multiply(PI).subtract(om);
-                BigDecimal t2 = k2.add(j1).multiply(PI).subtract(om);
+                double t1 =(((-k2)+j1)*Math.PI + om);
+                double t2 =(((k2)+j1)*Math.PI + om);
+                
                 this.times.add(t1);
                 this.times.add(t2);
                 this.time1.add(t1);
@@ -104,13 +108,13 @@ public class Knot {
     public void generatePairDT(){
         for(int i=0;i<times.size();i+=2){
             if(time1.contains(times.get(i))){
-                if(compare(getZ(times.get(i)), getZ(time2.get(time1.indexOf(times.get(i)))))){
+                if(getZ(times.get(i))>getZ(time2.get(time1.indexOf(times.get(i))))){
                     DT+=" "+(times.indexOf(time2.get(time1.indexOf(times.get(i))))+1);
                 }else{
                     DT+=" "+(times.indexOf(time2.get(time1.indexOf(times.get(i))))+1)*(-1);
                 }
             }else{
-                if(compare(getZ(times.get(i)),getZ(time1.get(time2.indexOf(times.get(i)))))){
+                if(getZ(times.get(i))>getZ(time1.get(time2.indexOf(times.get(i))))){
                     DT+=" "+(times.indexOf(time1.get(time2.indexOf(times.get(i))))+1);
                 }else{
                     DT+=" "+(times.indexOf(time1.get(time2.indexOf(times.get(i))))+1)*(-1);
@@ -148,12 +152,12 @@ public class Knot {
         return big;
     }
     
-    public Point3d getPoint(BigDecimal t,int id){ //vrací bod - vstupní parametry čas a ID bodu
-        return new Point3d(x.getValue(t),y.getValue(t),z[0].getValue(t).add(z[1].getValue(t)),t,id);
+    public Point3d getPoint(double t,int id){ //vrací bod - vstupní parametry čas a ID bodu
+        return new Point3d(x.getValue(t),y.getValue(t),z[0].getValue(t)+z[1].getValue(t),t,id);
     }
     
-    public BigDecimal getZ(BigDecimal t){
-        return z[0].getValue(t).add(z[1].getValue(t));
+    public double getZ(double t){
+        return (z[0].getValue(t)+z[1].getValue(t));
     }
     
     public int crossNumber(){ //získání počtu průsečíků
--- src/bakalarka_0/pkg2/Point3d.java
+++ src/bakalarka_0/pkg2/Point3d.java
@@ -4,13 +4,13 @@ package bakalarka_0.pkg2;
 import java.math.BigDecimal;
 
 public class Point3d {
-    BigDecimal x;
-    BigDecimal y;
-    BigDecimal z;
-    BigDecimal t;
+    double x;
+    double y;
+    double z;
+    double t;
     int id;
     //vlastní třída pro 3D bod - drží si hlavní parametry (x,y,z), čas a ID 
-    public Point3d(BigDecimal x, BigDecimal y, BigDecimal z,BigDecimal t,int id){
+    public Point3d(double x, double y, double z,double t,int id){
         this.x=x;
         this.y=y;
         this.z=z;
